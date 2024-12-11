e482329f3e063435f49490419748fce7560af2cb
merhmood <mahmudsuberu9@gmail.com>
2024-12-11 11:56:18 +0100

    feat: change AF masterclass duration to 2 weeks

--- netlify/functions/payment.ts
+++ netlify/functions/payment.ts
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable import/no-anonymous-default-export */
-import type { Context } from "@netlify/functions";
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import type { Context } from '@netlify/functions';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.apiKey,
-  authDomain: "llmvikings.firebaseapp.com",
-  projectId: "llmvikings",
-  storageBucket: "llmvikings.firebasestorage.app",
-  messagingSenderId: "612818568614",
-  appId: "1:612818568614:web:1a9a4d1ddc995d3db8fb67",
-  measurementId: "G-H7VT2B93Z0",
+  authDomain: 'llmvikings.firebaseapp.com',
+  projectId: 'llmvikings',
+  storageBucket: 'llmvikings.firebasestorage.app',
+  messagingSenderId: '612818568614',
+  appId: '1:612818568614:web:1a9a4d1ddc995d3db8fb67',
+  measurementId: 'G-H7VT2B93Z0',
 };
 
 // Initialize Firebase
@@ -24,32 +24,34 @@ interface FormData {
   name: string;
   email: string;
   amount: string;
+  duration: string;
   date: string;
   tx_id: string;
 }
 
 export default async (req: Request, context: Context) => {
-  if (req.method !== "POST") {
+  if (req.method !== 'POST') {
     return {
       statusCode: 405,
-      body: JSON.stringify({ message: "Method Not Allowed" }),
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
     };
   }
 
   try {
     const requestText = await req.text();
-    const body: FormData = JSON.parse(requestText || "");
+    const body: FormData = JSON.parse(requestText || '');
 
-    const { name, email, amount, date, tx_id } = body;
+    const { name, email, duration, amount, date, tx_id } = body;
 
     console.log(`secret key - ${process.env.apiKey}`);
     console.log(name, email, date, tx_id);
 
     // Add the form details to the "waitlist" collection
-    await addDoc(collection(db, "masterclass-payment"), {
+    await addDoc(collection(db, 'masterclass-payment'), {
       name,
       email,
       amount,
+      duration,
       date,
       tx_id,
       timestamp: new Date(), // Optionally store the submission time
@@ -58,7 +60,7 @@ export default async (req: Request, context: Context) => {
     return new Response(
       JSON.stringify({
         statusCode: 200,
-        body: JSON.stringify({ message: "Form submitted successfully" }),
+        body: JSON.stringify({ message: 'Form submitted successfully' }),
       })
     );
   } catch (error) {
@@ -66,7 +68,7 @@ export default async (req: Request, context: Context) => {
       return new Response(
         JSON.stringify({
           statusCode: 500,
-          body: JSON.stringify({ error: "Internal Server Error" }),
+          body: JSON.stringify({ error: 'Internal Server Error' }),
         })
       );
   }
